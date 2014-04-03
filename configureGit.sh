184084d0a682690cda06ad77022ff7ae5c725986
Brett Young <byoung08@calpoly.edu>
2014-04-03 13:05:52 -0700

    added a documentation

--- configureGit.sh
+++ configureGit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script configures git whenever you start working 
+#on multiple computers. Run when using git on other computers.
 git config --global user.name "Brett Young"
 git config --global user.email "byoung08@calpoly.edu"
 git config --global color.ui "auto"
