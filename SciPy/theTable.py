2696cb676bc41420763cd76ebd20e76712a8a96d
Brett Young <byoung08@calpoly.edu>
2014-05-23 22:20:34 -0700

    ode and integration ex

---
+++ SciPy/theTable.py
@@ -0,0 +1,7 @@
+
+object    terminal vel    initial height  initial vel  initial angle
+
+
+golfball     44 m/s              0 m         90 m/s       45deg
+baseball     43 m/s             1.5 m        94 m/s       45deg 
+pumpkin      107 m/s            1.5 m        52 m/s       45deg
